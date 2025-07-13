3733222249d851f694a433be621bfc982aa5b120
M16yusuf <yusufsmd58@gmail.com>
2025-07-14 00:09:00 +0700

    feat: add task5 week2 weekly, need to debug furhter

--- index.js
+++ index.js
@@ -7,28 +7,94 @@ import { catchLogin, asyncLogin, tryLogin } from "./src/task1.js";
 import { getDataFromServer, processData } from "./src/task2.js";
 import { dapatkanData } from "./src/task3.js";
 import { divideAndsort } from "./src/task4.js";
+import { bacaData, tulisData, ubahData, hapusData } from "./src/task5.js";
 
-// nomor 1
-const testUser = "yusuf";
-const testPass = "admin";
-asyncLogin(testUser, testPass);
-catchLogin(testUser, testPass);
-tryLogin(testUser, testPass);
+// import readline promises 
+import * as readlinePromises from 'node:readline/promises';
 
-
-// nomor 2
-const testNo2 = true; 
-getDataFromServer(testNo2, processData);
+const rl = readlinePromises.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 
-// Nomor 3
-(async function(){
-    const url = "https://jsonplaceholder.typicode.com/users";
-    const resss = await dapatkanData(url);
-    console.log(resss);
+// // nomor 1
+// const testUser = "yusuf";
+// const testPass = "admin";
+// asyncLogin(testUser, testPass);
+// catchLogin(testUser, testPass);
+// tryLogin(testUser, testPass);
+
+
+// // nomor 2
+// const testNo2 = true; 
+// getDataFromServer(testNo2, processData);
+
+
+// // Nomor 3
+// (async function(){
+//     const url = "https://jsonplaceholder.typicode.com/users";
+//     const resss = await dapatkanData(url);
+//     console.log(resss);
+// })();
+
+
+// // nomor 4
+// console.log(divideAndsort(5956560159466056));
+// // output 56159466595656
+
+
+// nomor 5 
+(async function() {
+    const menuTable = [
+        { menu: "close menu"},
+        { menu: "Lihat data"},
+        { menu: "Tambah data"},
+        { menu: "Update data"},
+        { menu: "delete data"},
+        { menu: "Kembali ke menu"},
+    ];
+
+
+    // clear dan munculkan tabel menu
+    console.clear();
+    console.table(menuTable);
+    const result = await bacaData();
+
+    let close = false;
+    while (close == false){        
+        // menunggu inputan      
+        const menu = await rl.question('Apa yang akan anda lakukan? ');
+        switch(menu){
+            case "0":
+                close = true;
+                rl.close();
+                break;
+            case "1":
+                console.clear();
+                console.table(result); 
+                console.table(menuTable);   
+                break;
+            case "2":
+                console.table(menuTable);
+                const addData = await rl.question('Masukan catatan baru : ');
+                tulisData(addData);
+                break;
+            case "3":
+                console.table(result); 
+                const noDataubah = await rl.question('nomor yang mau dirubah : ');
+                const changeDat = await rl.question('Masukan catatan baru : ');
+                ubahData(noDataubah, changeDat);
+                break;
+            case "4":
+                console.table(result); 
+                const noDathapus = await rl.question('nomor yang mau dihapus : ');
+                hapusData(noDathapus);
+                break;
+            case "5":
+                console.clear();
+                console.table(menuTable);
+                break;
+        }
+    }
 })();
-
-
-// nomor 4
-console.log(divideAndsort(5956560159466056));
-// output 56159466595656
--- src/task5.js
+++ src/task5.js
@@ -7,21 +7,82 @@
  * path - untuk hal-hal yang berkaitan dengan file paths
  */
 
-import fs from 'fs';
-/**
- * fs.readfile, fs.readFileSync
- * fs.writeFile, fs.writeFIleSync
- * fs.mkdir, fs.mkdirSync
- */
+import  fs  from 'fs';
 
-function readFile (){
-    
+// path for ESmodule
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const directoryPath = path.join(__dirname, '../data/');
+const fileName = 'my_database.json';
+const filePath = path.join(directoryPath, fileName);
+
+const dataBaseBaru =
+    `[
+    {"no":0,"catatan":"database baru"}
+]`;
+
+// function untuk bacadata my_database.json
+async function bacaData() {
+    // coba baca file jika tidak ada maka buat baru
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        // console.log('File content:', data);
+        const jsonData = await JSON.parse(data);
+        // console.log(jsonData);
+        return jsonData;
+    } catch (err) {
+        // buat file atau directory jika tidak ada
+        console.error('Error reading file: direktori tidak ditemukan');
+        fs.mkdirSync(directoryPath, { recursive: true });
+        fs.writeFileSync(filePath, dataBaseBaru);
+        console.log("Buat database baru...");
+
+        console.error('Error reading file:', err);
+    }
+
 }
 
-function writeFIle(){
-
+// function untuk tambah/tulisData my_database.json
+async function tulisData(inputData) {
+    try{
+        // ambil max data currentData, dan masukan info sebagai no
+        // dan catatan dengan inputData, lalu push newData ke currentData
+        const test = await bacaData();
+        const newData = { no: test.length, catatan: inputData };
+        test.push(newData);
+        fs.writeFileSync(filePath, JSON.stringify(test));
+    }catch(err){
+        console.error(err);
+    }
 }
 
-function updateFile(){
-    
+
+
+// function untuk ubahdata my_database.json
+async function ubahData(noData, inputUbah) {
+    try{
+        const test = await bacaData();
+        test[noData].catatan = inputUbah;
+        fs.writeFileSync(filePath, JSON.stringify(test));
+    }catch(err){
+        console.error(err);
+    }
 }
+
+
+// function untuk hapusdata my_database.json
+async function hapusData(noHapus) {
+    try{
+        const test = await bacaData();
+        const newData = test.splice(noHapus, 1);
+        fs.writeFileSync(filePath, JSON.stringify(test));
+    } catch(err){
+        console.error(err);
+    }
+}
+
+export { bacaData, tulisData, ubahData, hapusData };
