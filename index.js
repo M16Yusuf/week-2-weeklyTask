e84a6510ca090010c3f6c8a0537eeaa3b87321ed
M16yusuf <yusufsmd58@gmail.com>
2025-07-14 07:39:39 +0700

    fix: change menu  inteface for index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  *    "Muhammad Yusuf"
  *    Bootcamp Koda batch 3 golang fullstack
  */
+import { clear } from "node:console";
 import { catchLogin, asyncLogin, tryLogin } from "./src/task1.js";
 import { getDataFromServer, processData } from "./src/task2.js";
 import { dapatkanData } from "./src/task3.js";
@@ -18,30 +19,35 @@ const rl = readlinePromises.createInterface({
 });
 
 
-// // nomor 1
-// const testUser = "yusuf";
-// const testPass = "admin";
-// asyncLogin(testUser, testPass);
-// catchLogin(testUser, testPass);
-// tryLogin(testUser, testPass);
+async function jawabanLain() {
+    // nomor 1
+    // penggunana then-catch, async await, dan try catch
+    const testUser = "yusuf";
+    const testPass = "admin";
+    asyncLogin(testUser, testPass);
+    catchLogin(testUser, testPass);
+    tryLogin(testUser, testPass);
+
+    
+    // nomor 2
+    // print perulangan product timeout 3 detik
+    const testNo2 = true; 
+    getDataFromServer(testNo2, processData);
 
 
-// // nomor 2
-// const testNo2 = true; 
-// getDataFromServer(testNo2, processData);
+    // Nomor 3
+    // output nama dan domisili
+    (async function(){
+        const url = "https://jsonplaceholder.typicode.com/users";
+        const resss = await dapatkanData(url);
+        console.log(resss);
+    })();
 
 
-// // Nomor 3
-// (async function(){
-//     const url = "https://jsonplaceholder.typicode.com/users";
-//     const resss = await dapatkanData(url);
-//     console.log(resss);
-// })();
-
-
-// // nomor 4
-// console.log(divideAndsort(5956560159466056));
-// // output 56159466595656
+    // nomor 4
+    // output 56159466595656
+    console.log(divideAndsort(5956560159466056));
+}
 
 
 // nomor 5 
@@ -53,18 +59,21 @@ const rl = readlinePromises.createInterface({
         { menu: "Update data"},
         { menu: "delete data"},
         { menu: "Kembali ke menu"},
+        { menu: "tampil jawaban lain"},
     ];
 
-
-    // clear dan munculkan tabel menu
-    console.clear();
-    console.table(menuTable);
-    const result = await bacaData();
-
     let close = false;
-    while (close == false){        
+    while (close == false){
+        // panggil data terlebih dahulu dan kasih delay
+        console.log("loading.....");
+        const result = await bacaData();
+        await new Promise(resolve => setTimeout(resolve, 1000)); 
+        // clear dan munculkan tabel menu
+        console.clear();
+        console.table(menuTable);
+
         // menunggu inputan      
-        const menu = await rl.question('Apa yang akan anda lakukan? ');
+        let menu = await rl.question('Apa yang akan anda lakukan? ');
         switch(menu){
             case "0":
                 close = true;
@@ -72,28 +81,48 @@ const rl = readlinePromises.createInterface({
                 break;
             case "1":
                 console.clear();
+                console.log("|=========== [ DATABASE ] ============|");
                 console.table(result); 
-                console.table(menuTable);   
+                console.log(`input "5" kembali ke menu`);
+                console.log(`input "0" close program`);   
+                menu = await rl.question('Apa yang akan anda lakukan? ');
                 break;
             case "2":
-                console.table(menuTable);
+                console.clear();
+                console.log("|=========== [ DATABASE ] ============|");
+                console.table(result); 
                 const addData = await rl.question('Masukan catatan baru : ');
                 tulisData(addData);
+                // habis ubah/update balik ke menu
+                console.clear();
+                console.table(menuTable);
                 break;
             case "3":
+                console.clear();
+                console.log("|=========== [ DATABASE ] ============|");
                 console.table(result); 
                 const noDataubah = await rl.question('nomor yang mau dirubah : ');
                 const changeDat = await rl.question('Masukan catatan baru : ');
                 ubahData(noDataubah, changeDat);
+                // habis ubah/update balik ke menu
+                console.clear();
+                console.table(menuTable);
                 break;
             case "4":
                 console.table(result); 
                 const noDathapus = await rl.question('nomor yang mau dihapus : ');
                 hapusData(noDathapus);
+                // habis hapus balik ke menu
                 break;
             case "5":
                 console.clear();
                 console.table(menuTable);
+                break;
+            case "6":
+                await jawabanLain(); 
+                await new Promise(resolve => setTimeout(resolve, 4500)); 
+                console.log("|=================================|");
+                menu = await rl.question('input "5" kembali ke menu : ');
                 break;
         }
     }
